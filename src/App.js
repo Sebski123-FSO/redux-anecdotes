de60b8ed954b22221c8ae1ee5bbb0b7908aab113
sebski123 <sebthomle@gmail.com>
2022-02-24 00:13:57 +0100

    6.8: anecdotes, step6

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import { useDispatch, useSelector } from "react-redux";
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 import React from "react";
-import { vote } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const handleVote = (id) => {
-    dispatch(vote(id));
-  };
-
-  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      ))}
+      <AnecdoteList />
       <h2>create new</h2>
       <AnecdoteForm />
     </div>
